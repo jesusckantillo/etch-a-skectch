3dbb4a8ccb8a3f58061f0ab7a3fbda2d3de4b2f6
jesusckantillo <jesuscantillocode@gmail.com>
2023-06-26 00:15:06 -0500

    All requirements ready

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 let gridContainer = document.getElementById('grid-container');
+let gridCells;
+let changeColorButton = document.getElementById('btn-change-color');
+let rainbowModeButton = document.getElementById('btn-rainbow-mode');
+let eraserButton = document.getElementById('btn-eraser');
+
+let rainbowMode = false
+
+//Event handlers functions
+
+handleEraserButton = (e) => {
+  cleanGrid();
+}
+
+handleChangeColorButton = (e) => {
+  console.log(e.target);
+}
+
+handleRainbowModeButton = (e) => {
+  rainbowMode = !rainbowMode;
+  console.log("Rainbow mode: " + rainbowMode);
+  //Use "?" operator for change the color if the rainbowMode is true
+  rainbowMode ? rainbowModeButton.style.backgroundColor = "gray" : rainbowModeButton.style.backgroundColor =  "#2b3d4f";
+  
+}
 
 
+handleDraggingDraw = (e) => {
+  if(e.buttons === 1){
+   rainbowMode ? drawBlack(e.target,generateRandomColor()) :  drawBlack(e.target,"black")  ;
+  }
+}
+
+handleClickDraw = (e) => {
+ rainbowMode ?  drawBlack(e.target,generateRandomColor()) :  drawBlack(e.target,"black")  ;
+}
+
+
+
+
+
+
+
+
+
+//Event Listeners
+eraserButton.addEventListener('click', handleEraserButton);
+changeColorButton.addEventListener('click', handleChangeColorButton);
+rainbowModeButton.addEventListener('click',handleRainbowModeButton);
+
+function addGridEventListeners(cellsList){
+  cellsList.forEach(cell => {
+      cell.addEventListener('mouseover',handleDraggingDraw);
+      cell.addEventListener('click',handleClickDraw);
+  });
+}
+
+
+
+
+//Grid Functions
 function createGridContainer(gridCont){
     gridCont.style.display = "grid";
     gridCont.style.gridTemplateColumns = "repeat(16, 1fr)";
@@ -8,6 +66,13 @@ function createGridContainer(gridCont){
     gridCont.style.border = "0.5px solid black";
 }
 
+
+function cleanGrid(){
+  let gridCells = document.querySelectorAll('.grid-cell');
+  gridCells.forEach(cell => {
+    cell.style.backgroundColor = "white";
+  });
+}
 
 
 function createGrid(size) {
@@ -18,6 +83,31 @@ function createGrid(size) {
       gridContainer.appendChild(cell);
     }
   }
+   gridCells = document.querySelectorAll('.grid-cell');
+   addGridEventListeners(gridCells);
 }
+
+
+//Draw Functions
+
+function drawBlack(div, color){
+  div.style.backgroundColor = color;
+}
+
+
+function generateRandomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let color = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+  
+  return color;
+}
+
+
+
+
+
 createGridContainer(gridContainer);
 createGrid(16);
+console.log(rainbowMode)
